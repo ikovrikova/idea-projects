7b93d702e9a39442c675c3b9ba5752e8eb90c72d
ikovrikova <ievgeniiakovrikova@gmail.com>
2019-02-21 23:03:34 +0200

    Add files via upload

---
+++ idea_projects/src/com/example/firstpackage/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.firstpackage;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello it's me!");
+    }
+
+}
